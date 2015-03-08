4d085c31d3b924cee29b06d4c4220998606a70ec
Robert Gardner <rhg259@nyu.edu>
2015-03-07 19:22:27 -0500

    Add height, width to video, tune green screen params

--- main.js
+++ main.js
@@ -33,7 +33,8 @@ let processor = {
       let r = frame.data[i * 4 + 0];
       let g = frame.data[i * 4 + 1];
       let b = frame.data[i * 4 + 2];
-      if (g > 100 && r > 100 && b < 43)
+      // Tune these parameters to match the green screen.
+      if (g > 80 && r > 0 && b < 100)
         frame.data[i * 4 + 3] = 0;
     }
     this.ctx2.putImageData(frame, 0, 0);
